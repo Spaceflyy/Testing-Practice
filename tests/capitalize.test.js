42f14563f047f8bc4b66fb172d8971acb383e03f
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-09-26 18:30:54 +0100

    removed unneeded file

--- tests/capitalize.test.js
+++
@@ -1,9 +0,0 @@
-import { capitalize, reverseWord } from "../js/index.js";
-
-test("Capitalize first letter of word", () => {
-	expect(capitalize("hello")).toBe("Hello");
-});
-
-test("Reverse a string", () => {
-	expect(reverseWord("test")).toBe("tset");
-});
